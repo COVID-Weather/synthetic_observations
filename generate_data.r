44672124a168127ea2251e5ef8dbd1a1a9ac7cdf
gbritten <gregleebritten@gmail.com>
2020-04-01 18:51:15 -0400

    different model options

--- generate_data.r
+++ generate_data.r
@@ -4,15 +4,18 @@ Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
 
 setwd('d:/dropbox/working/covid19/synthetic_observations/' )
 
+##############################################################
+## GENERATE DATA #############################################
+##############################################################
 niter <- 100
 dt    <- 1
 S=E=I=R=t <- numeric(niter/dt)
 I[1] <- 0.001
 S[1] <- 1.0 - I[1]
 
-gamma <- 0.1
+gamma <- 0.2
 beta <- 0.6
-sigma <- 0.1
+sigma <- 0.2
 
 for(i in 2:niter){
 	dSdt <- -beta*S[i-1]*I[i-1]
@@ -44,8 +47,8 @@ R_obs0 <- rpois(N_obs,lambda=R[t_obs]*POP)
 
 ##--MAKE A PLOT OF OBSERVATIONS--##############
 par(mfrow=c(2,2),mar=c(2,2,2,2))
-plot(S,type='l')
-	par(new=TRUE)
+plot(S,type='l'); 
+	par(new=TRUE); 
 	plot(S_obs0,yaxt='n',xaxt='n')
 	axis(side=4)
 plot(E,type='l')
@@ -68,27 +71,123 @@ I0 <- 0.001
 S0 <- 1.0 - I0
 R0 <- 0.0
 E0 <- 0.0
-
 x0 <- c(S0,E0,I0,R0) #initial conditions
 
-data <- list(POP=POP,y=cbind(S_obs0,E_obs0,I_obs0,R_obs0),N_obs=N_obs,t_obs=t_obs,x0=x0)
+num <- 0.6
+data <- list(POP=POP,
+		     y=cbind(S_obs0,E_obs0,as.integer(num*I_obs0),as.integer(num*R_obs0)),
+			 N_obs=N_obs,
+			 t_obs=t_obs,
+			 x0 = x0,
+			 x_p=4,
+			 N_obsvar=1,
+			 theta_p=3,
+			 i_obsvar=c(3))
+opt_SEIR <- optimizing(mod_SEIR,data=data)
+opt_SEIR$par[1:3]
+opt_SEIR$par[2]/opt_SEIR$par[1]
 
+
+
+
+##--COMPILE STAN CODE--###############
 mod_SEIR <- stan_model('stan_SIER.stan') #compile the stan code
+#mod_SEIR <- stan_model('stan_SIER_multinomial.stan')
   
+##--MCMC VS OPTIMIZATION--############
 mcmc_SEIR <- sampling(mod_SEIR,data=data,open_progress=TRUE) #mcmc
 
 post_SEIR <- extract(mcmc_SEIR) #extract samples
+post_SEIR_I_R <- extract(mcmc_SEIR_I_R)
 
-##--PLOT THE FIT--###################
-par(mfrow=c(1,1))
-y_pred <- apply(post_SEIR$y_pred,c(2,3),mean) 
-matplot(y_pred,type='l',lty=1,ylim=c(0,1200))
-for(i in 1:4){
-	lines(apply(post_SEIR$y_pred[,,i],2,function(x) quantile(x,p=0.025)),col=i,lty=2)
-	lines(apply(post_SEIR$y_pred[,,i],2,function(x) quantile(x,p=0.975)),col=i,lty=2)
-	points(data$y[,i],col=i)
+
+####################################################################
+## LINEAR BIAS wrt TIME ############################################
+## - code assumes a linear ramp-up in the proportion of infected being tested
+## - p0 is the initial proportion of infecteds being observed 
+####################################################################
+
+n_sim <- 20
+p0s <- seq(0,0.9,length.out=n_sim)
+PARS <- matrix(NA,nrow=3,ncol=n_sim)
+SDS  <- matrix(NA,nrow=3,ncol=n_sim)
+
+for(i in 1:20){
+	data_I_R_test <- data_I_R
+	data_I_R_test$y[,3] <- as.integer(seq(p0s[i],1,length.out=N_obs)*data_I_R_test$y[,3])
+	
+	opt_SEIR_I_R_test <- optimizing(mod_SEIR,data=data_I_R_test,init='0',hessian=TRUE)
+	PARS[,i] <- opt_SEIR_I_R_test$par[1:3]
+	SDS[,i] <- sqrt(diag(solve(-opt_SEIR_I_R_test$hessian)))
 }
-legend('top',legend=c('S','E','I','R'),col=1:4,lty=1,bty='n')
 
 
+####################################################################
+## PIECE-WISE BIAS #################################################
+####################################################################
+n_sim_pw <- 20
+p0s_pw <- seq(0,0.9,length.out=n_sim_pw)
+PARS_pw <- matrix(NA,nrow=3,ncol=n_sim_pw)
+SDS_pw  <- matrix(NA,nrow=3,ncol=n_sim_pw)
+
+for(i in 1:20){
+	data_I_R_test <- data_I_R
+	data_I_R_test$y[,3] <- as.integer(c(rep(p0s[i],10),rep(1,10))*data_I_R_test$y[,3])
+	
+	opt_SEIR_I_R_test <- optimizing(mod_SEIR,data=data_I_R_test,init='0',hessian=TRUE)
+	PARS_pw[,i] <- opt_SEIR_I_R_test$par[1:3]
+	SDS_pw[,i] <- sqrt(diag(solve(-opt_SEIR_I_R_test$hessian)))
+}
+
+
+par(mfrow=c(2,2),mar=c(3,3,3,3),oma=c(2,2,2,2))
+plot(seq(p0s[1],1,length.out=N_obs),type='l')
+for(i in 2:length(p0s)) lines(seq(p0s[i],1,length.out=N_obs))
+mtext(side=2,'Testing bias',line=2.5)
+plot(p0s,PARS[1,],ylim=c(0.2,1.1),type='l',col='blue')
+	lines(p0s,PARS[1,] + 2*SDS[1,],lty=2,col='blue')
+	lines(p0s,PARS[1,] - 2*SDS[1,],lty=2,col='blue')
+	legend('topright',legend=c(expression(italic(gamma)),
+	                           expression(italic(beta)),
+							   expression(italic(R['0']))),bty='n',lty=1,col=c('blue','red','black'))
+lines(p0s,PARS[2,],col='red')
+	lines(p0s,PARS[2,] + 2*SDS[2,],lty=2,col='red')
+	lines(p0s,PARS[2,] - 2*SDS[2,],lty=2,col='red')
+abline(h=gamma,col='blue')
+abline(h=beta,col='red')
+par(new=TRUE)
+plot(p0s,PARS[2,]/PARS[1,],ylim=c(2.5,4),xaxt='n',yaxt='n',type='l')
+	lines(p0s,PARS[2,]/PARS[1,] + 2*sqrt(SDS[1,]^2 + SDS[2,]^2),lty=2)
+	lines(p0s,PARS[2,]/PARS[1,] - 2*sqrt(SDS[1,]^2 + SDS[2,]^2),lty=2)
+axis(side=4)
+lines(p0s,PARS[3,])
+abline(h=beta/gamma)
+mtext(side=1,'Initial testing bias',line=2.5)
+
+
+
+#par(mfrow=c(1,2),mar=c(3,3,3,3),oma=c(2,2,2,2))
+plot(c(rep(p0s_pw[1],50),rep(1,50)),type='l')
+for(i in 2:length(p0s_pw)) lines(c(rep(p0s_pw[i],50),rep(1,50)))
+mtext(side=2,'Testing bias',line=2.5)
+
+plot(p0s_pw,PARS_pw[1,],ylim=c(0.2,1.1),type='l',col='blue')
+	lines(p0s_pw,PARS_pw[1,] + 2*SDS_pw[1,],lty=2,col='blue')
+	lines(p0s_pw,PARS_pw[1,] - 2*SDS_pw[1,],lty=2,col='blue')
+	legend('topright',legend=c(expression(italic(gamma)),
+	                           expression(italic(beta)),
+							   expression(italic(R['0']))),bty='n',lty=1,col=c('blue','red','black'))
+lines(p0s_pw,PARS_pw[2,],col='red')
+	lines(p0s_pw,PARS_pw[2,] + 2*SDS_pw[2,],lty=2,col='red')
+	lines(p0s_pw,PARS_pw[2,] - 2*SDS_pw[2,],lty=2,col='red')
+abline(h=gamma,col='blue')
+abline(h=beta,col='red')
+par(new=TRUE)
+plot(p0s_pw,PARS_pw[2,]/PARS_pw[1,],ylim=c(2.5,4),xaxt='n',yaxt='n',type='l')
+	lines(p0s,PARS_pw[2,]/PARS_pw[1,] + 2*sqrt(SDS_pw[1,]^2 + SDS_pw[2,]^2),lty=2)
+	lines(p0s,PARS_pw[2,]/PARS_pw[1,] - 2*sqrt(SDS_pw[1,]^2 + SDS_pw[2,]^2),lty=2)
+axis(side=4)
+lines(p0s_pw,PARS_pw[3,])
+abline(h=beta/gamma)
+mtext(side=1,'Initial testing bias',line=2.5)
 
--- stan_SIER.stan
+++ stan_SIER.stan
@@ -5,9 +5,10 @@ functions {
                real[] x_r,
                int[]  x_i) {       // parameters
 
-    real gamma = theta[1];    
-    real beta  = theta[2]; 
-    real sigma = theta[3]; 
+	
+    //real gamma = theta[1];    
+    real beta  = theta[1]; 
+    real sigma = theta[2]; 
     
     real S = fmax(x[1],0.0);
     real E = fmax(x[2],0.0);
@@ -16,8 +17,8 @@ functions {
 
 	real dS = -beta*S*I;
 	real dE =  beta*S*I - sigma*E;
-	real dI = sigma*E - gamma*I;
-	real dR = gamma*I;
+	real dI = sigma*E - 0.1*I;
+	real dR = 0.1*I;
 
     return {dS,dE,dI,dR};
   }
@@ -26,39 +27,44 @@ data {
   real<lower=0> POP;           //total population size		
   int<lower=0>  N_obs;         // num obs
   real          t_obs[N_obs];          // obs times
-  int<lower=0>  y[N_obs,4]; // observed variable at measurement times
-  real          x0[4];
+  int           x_p;         // number of state variables
+  int           N_obsvar;    // number of observed variables
+  int<lower=0>  y[N_obs,x_p]; // observed variable at measurement times
+  int           theta_p;  //number of fitted parameters
+  int           i_obsvar[N_obsvar]; //indices for observations
+  //int           i_obsvar; //indices for observations
+  real          x0[x_p];
+//  int           N_pred;
+//  real          t_pred[N_pred];  
 }
 parameters {
-  real<lower = 0.0> theta[3];   // parameters
-  //real<lower = 0.0> x0[4];      // initial population
+  real<lower=0.0>           theta[2];   // parameters
+  //real<lower=0.0,upper=1.0> s0;
 }
-transformed parameters {	
-  real x[N_obs,4] = integrate_ode_rk45(SIER, x0, 0, t_obs, theta, rep_array(0.0,0), rep_array(0,0), 1e-8, 1e-8, 1e6);
+transformed parameters {
+  real x[N_obs,x_p];
+  x = integrate_ode_rk45(SIER, x0, 0, t_obs, theta, 
+		rep_array(0.0,0), rep_array(0,0), 1e-8, 1e-8, 1e6);
 }
 model {
-  //x0[1]    ~ uniform(0.0,1.0);
-  //x0[2]    ~ uniform(0.0,1.0);
-  //x0[3]    ~ uniform(0.0,1.0);
-  //x0[4]    ~ uniform(0.0,1.0);
-  theta[1] ~ normal(0.2,2);
-  theta[2] ~ normal(0.6,2);
-  theta[3] ~ normal(0.2,2);
+  // s0       ~ uniform(0.5,1.0);
+  //theta[1] ~ normal(0.3,0.5);
+  theta[1] ~ normal(0.3,0.5);
+  theta[2] ~ normal(0.3,0.5);
   
-  for(i in 1:4){
-	  //y[1:N_obs,i] ~ poisson(x[1:N_obs,i]*POP)
+  //for(i in i_obsvar){
 	  for(j in 1:N_obs){
-		y[j,i] ~ poisson(fmax(x[j,i]*POP,0.0));
+		y[j,3] ~ poisson(fmax(x[j,3]*POP,1E-15));
 	  }
-  }
+  //}
 }
 generated quantities {
-  real x_pred[N_obs,4]; 
-  real y_pred[N_obs,4];
-  x_pred = integrate_ode_rk45(SIER, x0, 0, t_obs, theta, rep_array(0.0,0), rep_array(0,0),1e-8,1e-8,1e6) ;
-  for(i in 1:4){
+  real x_pred[N_pred,x_p]; 
+  real y_pred[N_pred,x_p];
+  x_pred = integrate_ode_rk45(SIER, x0, 0, t_pred, theta, rep_array(0.0,0), rep_array(0,0),1e-8,1e-8,1e6);
+  for(i in 1:x_p){
 	  for(j in 1:N_obs){
-		  y_pred[j,i] = poisson_rng(fmax(x[j,i]*POP,0.0));
+		  y_pred[j,i] = poisson_rng(fmax(x[j,i]*POP,1E-15));
 	  }
   }
 }
